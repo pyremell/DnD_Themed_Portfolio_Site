00d077fcc6146c35e26dc5e24e1463ada700b693
Jacob Schaefer <pyremell@gmail.com>
2022-08-09 16:40:51 -0700

    Fixed padding on mobile. Removed SVG/JS parchment effect in favor of a more accurate textured background for the actual character sheet. Added autoplay and infinite scrolling to the projects carousel. A few other minor style tweaks.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,1 @@
-// First call
-ScrollHeight();
 
-// Redraw when viewport is modified
-window.addEventListener('resize', function (event) {
-  ScrollHeight();
-});
-
-function ScrollHeight() {
-  var content = document.querySelector('#parchment');
-  var container = document.querySelector('#contain');
-
-  // SVG feTurbulence can modify all others elements, that's why "parchment" is in absolute position.
-  // so for a better effect, absolute height is defined by his content.
-  content.style.height = container.offsetHeight + 'px';
-}
